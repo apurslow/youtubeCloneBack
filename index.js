0a283a9675a0db74793823b568f26336b6f3ae10
kingbsolomon <bjsolomon@gmail.com>
2022-06-16 10:12:05 -0400

    updates to routes and new installations

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const connectDB = require("./startup/db");
-const users = require("./routes/users");
-const posts = require("./routes/posts");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+        cors: {
+            origin: "http://localhost:3000"
+        }});
+
+const users = require("./routes/users");
+const posts = require("./routes/posts")(io);
 
 connectDB();
 
@@ -14,6 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", users);
 app.use("/api/posts", posts);
-app.listen(5000, ()=>{
+server.listen(5000, ()=>{
     console.log("Sever Started Port 5000")
 });
--- routes/posts.js
+++ routes/posts.js
@@ -1,67 +1,81 @@
-const { Post, validatePost} = require("../models/post");
+const { Post, validatePost } = require("../models/post");
 const express = require("express");
 const router = express.Router();
 
-// Get all posts/comments
-router.get("/", async (req, res) => {
-    try {
-      const posts = await Post.find();
-      return res.send(posts);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-});
-
-//get all posts by videoId
-router.get("/:videoId", async (req, res) => {
-    try {
-        const posts = await Post.find({videoId: req.params.videoId});
-        return res.send(posts);
-      } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`);
-      }
-});
+module.exports = function (io) {
 
 
-//for a user to create a new post
-router.post("/", async (req, res) => {
-    try {
-      const { error } = validatePost(req.body);
-      if (error) return res.status(400).send(error.details[0].message);
-  
-      let newPost = await new Post(req.body);
-      await newPost.save()
-      return  res.status(201).send(newPost)
-    } catch (ex){
-      return res.status(500).send(`Internal Server Error: ${ex}`)
-  }
-  });
+    io.on("connection", function (socket) {
+        // Everytime a client logs in, display a connected message
+        console.log("Server-Client Connected!");
+    });
 
-//for a user to update their post
-router.put("/:postId", async (req, res) => {
-try {
-    const { error } = validatePost(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    // Get all posts/
+    router.get("/", async (req, res) => {
+        try {
+            let response = await Post.find();
+            return res.send(response);
+        } catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`);
+        }
+    });
 
-    let posts = await Post.findByIdAndUpdate(req.params.postId,req.body,{new:true});
-    if(!posts) return res.status(400).send(`Could not find any comments with the ID of ${req.params.postId}`)
-} catch (ex){
-    return res.status(500).send(`Internal Server Error: ${ex}`)
-}
-});
-
-
-//for a user to delete their post
-router.delete("/:postId", async (req, res) => {
-    try {
-        let posts = await Post.findByIdAndDelete(req.params.postId);
-        if(!posts) return res.status(400).send(`Could not find any comments with the ID of ${req.params.postId}`)
-    } catch (ex){
-        return res.status(500).send(`Internal Server Error: ${ex}`)
-    }
+    //get all posts by videoId
+    router.get("/:videoId", async (req, res) => {
+        try {
+            const posts = await Post.find({ videoId: req.params.videoId });
+            return res.send(posts);
+        } catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`);
+        }
     });
 
 
-module.exports = router;
+    //for a user to create a new post
+    router.post("/", async (req, res) => {
+        try {
+            const { error } = validatePost(req.body);
+            if (error) return res.status(400).send(error.details[0].message);
+
+            let newPost = await new Post(req.body);
+            await newPost.save();
+            io.emit("new-post");
+            return res.status(201).send(newPost)
+        } catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`)
+        }
+    });
+
+    //for a user to update their post
+    router.put("/:postId", async (req, res) => {
+        try {
+            const { error } = validatePost(req.body);
+            if (error) return res.status(400).send(error.details[0].message);
+
+            let post = await Post.findByIdAndUpdate(req.params.postId, req.body, { new: true });
+            if (!post) return res.status(400).send(`Could not find any comments with the ID of ${req.params.postId}`)
+            io.emit("updated-post");
+            return res.send(posts);
+        } catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`)
+        }
+    });
+
+
+    //for a user to delete their post
+    router.delete("/:postId", async (req, res) => {
+        try {
+            let post = await Post.findByIdAndDelete(req.params.postId);
+            if (!post) return res.status(400).send(`Could not find any comments with the ID of ${req.params.postId}`);
+            io.emit("deleted-post");
+            return res.send("Post Deleted");
+        } catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`)
+        }
+    });
+    return router;
+
+};
+
 
 
